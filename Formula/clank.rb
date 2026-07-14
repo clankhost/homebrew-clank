96f53eef0f6086e10d95ef48299b756efc0f2011
Austin Naremore <austin@naremore.com>
2026-07-13 20:04:57 -0500

    Update to v0.3.13

--- Formula/clank.rb
+++ Formula/clank.rb
@@ -1,28 +1,28 @@
 class Clank < Formula
   desc "CLI for the Clank deployment platform"
   homepage "https://clank.host"
-  version "0.3.12"
+  version "0.3.13"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/clankhost/clank-cli/releases/download/v0.3.12/clank_darwin_arm64.tar.gz"
-      sha256 "2f01d72fc3da90c9610ef8db2ff18583240337e56cef8ed432c7158c38a65a73"
+      url "https://github.com/clankhost/clank-cli/releases/download/v0.3.13/clank_darwin_arm64.tar.gz"
+      sha256 "cf4d36ddc6e71068cccd1a1c31cc91007223952bd18698c7e2056ff11da1c7b4"
     end
     on_intel do
-      url "https://github.com/clankhost/clank-cli/releases/download/v0.3.12/clank_darwin_amd64.tar.gz"
-      sha256 "6ec67df71e31907963d96d0257aea9562a7f5944bc87bc9bad277045e5fd4854"
+      url "https://github.com/clankhost/clank-cli/releases/download/v0.3.13/clank_darwin_amd64.tar.gz"
+      sha256 "6bd618c6b8a6b93319fe7c0d1b265cab1dd359e04e7a987396bea82a32819cb5"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/clankhost/clank-cli/releases/download/v0.3.12/clank_linux_arm64.tar.gz"
-      sha256 "c02fe50d4eebf122506901aae71eab4df5df97ce9f0e9608118471a8dc59757a"
+      url "https://github.com/clankhost/clank-cli/releases/download/v0.3.13/clank_linux_arm64.tar.gz"
+      sha256 "8021ea03299f423006d72bfbce36511bad62128824a361a180e405ae24563ef6"
     end
     on_intel do
-      url "https://github.com/clankhost/clank-cli/releases/download/v0.3.12/clank_linux_amd64.tar.gz"
-      sha256 "49bc99253011b6fbb0bc87181c13ed97597941164a834551b53ad90f62fa314d"
+      url "https://github.com/clankhost/clank-cli/releases/download/v0.3.13/clank_linux_amd64.tar.gz"
+      sha256 "25fa0f85b7f9e984e079accff0857390676b2a025ecea89e96c972987f4dfd77"
     end
   end
 
